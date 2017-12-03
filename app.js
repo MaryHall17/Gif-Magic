2ac673b1970d04d67f4f40adefc84d9409ef0678
Mary Hall <mh6uj@students-MBP.home>
2017-12-03 17:44:10 -0500

    ajax call retrieves data on click

--- app.js
+++ app.js
@@ -14,17 +14,32 @@
 //Empty the gifs at each button; no prepending or appending. .empty()?
 //Limit 10 gifs
 
-//Api key
-var magicKey = "YS9cNoPBG3S3ShQcI9FQwNycGpX342a7";
+//Display gif
+function displayGif() {
+	//data from for-loop inside renderButton function. Search paramter:
+	var search = $(this).attr("data-name");
+	//rating paramater
+	var rating = "G";
+	//Api key
+	var magicKey = "YS9cNoPBG3S3ShQcI9FQwNycGpX342a7";
+	//URL:
+	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=YS9cNoPBG3S3ShQcI9FQwNycGpX342a7&q=" + search + "&limit=10&offset=0&rating=G&lang=en";
 
-//search paramater
-var q = $("#user-input")
+	//Ajax call for clicked topic button
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response) {
+		console.log(search);
+		$("#Gif-view").html(JSON.stringify(response.data.images));
+	});
+} 
 
-//rating paramater
-var rating = "G";
+
+
 //queryUrl
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=YS9cNoPBG3S3ShQcI9FQwNycGpX342a7&q=pizza&limit=10&offset=0&rating=G&lang=en"
+
 //Topic array for button values. 
 var topics = ["candy canes", "gingerbread", "penguins", "santa", "snow", "nutcracker", "the grinch"];
 
@@ -37,7 +52,7 @@ var topics = ["candy canes", "gingerbread", "penguins", "santa", "snow", "nutcra
 			//Generate buttons for each topic in the array
 			var button = $("<button>");
 			//Add a class of topics to button
-			button.addClass("btn btn-info");
+			button.addClass("btn btn-success");
 			//Add a data-attribute to later grab outside of this function
 			button.attr("data-name", topics[i]);
 			//Provide the button text as the topic name
@@ -57,6 +72,9 @@ var topics = ["candy canes", "gingerbread", "penguins", "santa", "snow", "nutcra
 		//Call the renderButton function to add new topics as buttons
 		renderButtons();
 	});
+
+	//Add a click listening event for the topic buttons
+	$(document).on("click", ".btn-success", displayGif);
 
 	//Calling the renderButtons to display the initial buttons
 	renderButtons();
